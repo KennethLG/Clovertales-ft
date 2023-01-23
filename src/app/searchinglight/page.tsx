3c3a4f93e1ef4d645cea001baef579a25a177402
KennethLG <orsonleiziaga@gmail.com>
2023-01-22 19:39:11 -0500

    Add the another tabs

---
+++ src/app/searchinglight/page.tsx
@@ -0,0 +1,9 @@
+const SearchingLight = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-5xl font-bold">Searching Light</h1>
+    </div>
+  );
+};
+
+export default SearchingLight;
---
+++ src/app/team/page.tsx
@@ -0,0 +1,9 @@
+const Team = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-5xl font-bold">Team</h1>
+    </div>
+  );
+};
+
+export default Team;
