9981c440e689a82cc7ff5ea97ecb967e3c3865a5
KennethLG <orsonleiziaga@gmail.com>
2023-02-01 18:58:19 -0500

    Resize Snow background

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -8,16 +8,19 @@ const Blog = () => {
     <section className="w-11/12 md:max-w-3xl m-auto mt-32">
       <h1 className="text-5xl my-4 text-center">Blog</h1>
       <Image
-        src={`${config.aws.cdn}/snowBg.png`}
+        src={`${config.aws.cdn}/snowBg-small.png`}
         alt="Searching Light snow background"
         width={200}
         height={200}
-        className="h-auto w-full md:w-3/5 m-auto rounded-2xl"
+        loading="lazy"
+        className="h-auto w-full md:w-4/5 m-auto rounded-2xl"
       />
 
       <p className="mt-10 text-center">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
+        tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor
+        sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+        ut labore et dolore magna aliqua.
       </p>
 
       {posts.map((post, i) => (
