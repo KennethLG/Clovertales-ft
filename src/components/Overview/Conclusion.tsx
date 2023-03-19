cd29686854c59e9e3b5a7beaff90f9a23468a57d
KennethLG <orsonleiziaga@gmail.com>
2023-03-18 22:41:35 -0500

    Add snow background at Conclusion.tsx

--- src/components/Overview/Conclusion.tsx
+++ src/components/Overview/Conclusion.tsx
@@ -1,11 +1,24 @@
+import { config } from "@/config";
+import { Image } from "../global/Image/Image";
+
 export const Conclusion = () => {
   return (
-    <p className="text-center">
-      Dive into the amazing universe of <strong>Searching Light</strong> and
-      accompany our brave hero in his quest to uncover the mysteries of these
-      parallel worlds. In this exciting adventure full of dangers and surprises,
-      only those with enough wit and determination will be able to unravel the
-      truth hidden in the shadows.
-    </p>
+    <>
+      <Image
+        alt="Searching Light logo"
+        src={`${config.aws.cdn}/snowBg-small.png`}
+        width={300}
+        height={300}
+        loading="lazy"
+        className="h-auto w-4/5 m-auto rounded-2xl"
+      />
+      <p className="text-center mt-5">
+        Dive into the amazing universe of <strong>Searching Light</strong> and
+        accompany our brave hero in his quest to uncover the mysteries of these
+        parallel worlds. In this exciting adventure full of dangers and
+        surprises, only those with enough wit and determination will be able to
+        unravel the truth hidden in the shadows.
+      </p>
+    </>
   );
-}
+};
