f1a0a14071f66802b3ee35fb2068160ac49c4112
KennethLG <orsonleiziaga@gmail.com>
2023-03-18 22:58:11 -0500

    Create GameplayGallery component

--- src/mocks/gallery.ts
+++ src/mocks/gallery.ts
@@ -3,5 +3,11 @@ import { config } from "@/config";
 export const gallery = [
   `${config.aws.cdn}/gallery/sl2.png`,
   `${config.aws.cdn}/gallery/sl1.png`,
-  `${config.aws.cdn}/gallery/sl3.png`
+  `${config.aws.cdn}/gallery/sl3.png`,
+  `${config.aws.cdn}/gallery/sl4.png`,
+  `${config.aws.cdn}/gallery/sl5.png`,
+  `${config.aws.cdn}/gallery/sl6.png`,
+  `${config.aws.cdn}/gallery/sl7.png`,
+  `${config.aws.cdn}/gallery/sl8.png`,
+  `${config.aws.cdn}/gallery/sl9.png`
 ];
