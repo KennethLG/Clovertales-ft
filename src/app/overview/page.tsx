e2e9216ec868a5c432d822a42800021a1aba39a7
KennethLG <orsonleiziaga@gmail.com>
2023-03-18 20:23:43 -0500

    Add features section

--- src/app/overview/page.tsx
+++ src/app/overview/page.tsx
@@ -1,9 +1,16 @@
+import { Card } from "@/components/global/Card/Card";
 import { SearchingLightGeneralDescription } from "@/components/Overview/Overview";
+import { cards } from "./data";
 
 const Overview = () => {
+
+
   return (
     <>
       <SearchingLightGeneralDescription />
+      {cards.map((card, i) => (
+        <Card key={card.title} {...card} reverse={i % 2 === 0} />
+      ))}
     </>
   );
 };
