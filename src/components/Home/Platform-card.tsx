a199e9e041614f22c3cb7e7361196151254f8910
KennethLG <orsonleiziaga@gmail.com>
2023-03-28 13:42:04 -0500

    Improve Platform-card.tsx

--- src/components/Home/Platform-card.tsx
+++ src/components/Home/Platform-card.tsx
@@ -1,5 +1,7 @@
+"use client";
 import { config } from "@/config";
 import Image from "next/image";
+import toast, { Toaster } from "react-hot-toast";
 
 type PlatformCardProps = {
   name: string;
@@ -8,22 +10,41 @@ type PlatformCardProps = {
 };
 
 export const PlatformCard = ({ name, iconName, link }: PlatformCardProps) => {
-  return (
-    <a
-      target="_blank"
-      href={link}
-      rel="noopener noreferrer"
-      className="w-1/2 md:w-1/3 lg:w-1/3 hover:text-fuchsia-400"
-    >
-      <div className="justify-around flex items-center p-1">
-        <Image
-          src={`${config.aws.cdn}/icons/${iconName}.png`}
-          width={80}
-          height={80}
-          alt={`${name} icon`}
-        />
-        <h1 className="text-xl">{name}</h1>
+  const onClick = () => {
+    toast.success(
+      <span>
+        <strong>{name}</strong> AVAILABLE SOON!
+      </span>
+    );
+  };
+
+  const Wrapper = ({ children }: { children: React.ReactNode }) => {
+    const commonProps = {
+      className:
+        "w-1/2 md:w-1/3 lg:w-1/3 hover:text-fuchsia-400 justify-around flex items-center p-1 cursor-pointer",
+    };
+
+    return link ? (
+      <a target="_blank" href={link} rel="noopener noreferrer" {...commonProps}>
+        {children}
+      </a>
+    ) : (
+      <div {...commonProps} onClick={onClick}>
+        {children}
       </div>
-    </a>
+    );
+  };
+
+  return (
+    <Wrapper>
+      <Image
+        src={`${config.aws.cdn}/icons/${iconName}.png`}
+        width={80}
+        height={80}
+        alt={`${name} icon`}
+      />
+      <h1 className="text-xl">{name}</h1>
+      <Toaster />
+    </Wrapper>
   );
 };
