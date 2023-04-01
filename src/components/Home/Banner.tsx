e9f3bd0db1af1c8a7dbe5fb556933968285c959f
KennethLG <orsonleiziaga@gmail.com>
2023-03-31 21:09:57 -0500

    Create gradient effect at Banner.tsx

--- src/components/Home/Banner.tsx
+++ src/components/Home/Banner.tsx
@@ -15,11 +15,11 @@ export const Banner = () => {
   return (
     <div className="relative">
       <Image
-        src={`${config.aws.cdn}/home/banner.png`}
+        src={`${config.aws.cdn}/home/banner-small.png`}
         alt="SearchingLight banner"
         className="h-auto w-full"
-        width={600}
-        height={600}
+        width={1000}
+        height={1000}
       />
 
       <Image
@@ -28,6 +28,11 @@ export const Banner = () => {
         className="h-auto w-3/5 absolute top-0 left-0 right-0 bottom-0 m-auto drop-shadow-2xl"
         width={1000}
         height={1000}
+      />
+
+      <div
+        className="w-full h-64 bottom-0 absolute bg-gradient-to-t from-black to-transparent"
+        style={{}}
       />
     </div>
   );
