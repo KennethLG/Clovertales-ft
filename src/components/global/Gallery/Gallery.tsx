c96bee9bbb8a3a7b0a3c06acad5bf36d1e65fd8f
KennethLG <orsonleiziaga@gmail.com>
2023-04-01 00:45:50 -0500

    Sort gallery

--- src/components/global/Gallery/Gallery.tsx
+++ src/components/global/Gallery/Gallery.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Card } from "@/domain/card";
 import useImagePopup from "@/hooks/useImagePopup";
@@ -14,23 +14,25 @@ export const Gallery = ({ images }: GalleryProps) => {
 
   return (
     <div className="flex flex-wrap my-5">
-      {images.map((image, i) => (
-        <div key={i} className="w-2/6 p-2">
-          <Image
-            alt={`Searching Light image-${i}`}
-            src={image.url}
-            width={300}
-            height={200}
-            onClick={() => openPopup(image.url, `Gallery Image ${i}`)}
-          />
-        </div>
-      ))}
-        <ImagePopup
-          src={popupImage?.src}
-          alt={popupImage?.alt}
-          onClose={closePopup}
-          isOpen={popupImage !== null}
-        />
+      {images
+        .sort((a, b) => a.order - b.order)
+        .map((image, i) => (
+          <div key={i} className="w-2/6 p-2">
+            <Image
+              alt={`Searching Light image-${i}`}
+              src={image.url}
+              width={300}
+              height={200}
+              onClick={() => openPopup(image.url, `Gallery Image ${i}`)}
+            />
+          </div>
+        ))}
+      <ImagePopup
+        src={popupImage?.src}
+        alt={popupImage?.alt}
+        onClose={closePopup}
+        isOpen={popupImage !== null}
+      />
     </div>
   );
 };
