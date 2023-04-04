c4d3b27e401863442780f97fd44d3574ac578b4d
KennethLG <orsonleiziaga@gmail.com>
2023-04-03 23:03:56 -0500

    Fix font size

--- src/components/Home/Subscribe/TextContent.tsx
+++ src/components/Home/Subscribe/TextContent.tsx
@@ -4,12 +4,13 @@ export default function TextContent() {
   return (
     <div className="w-full md:w-3/5 pt-1 md:pt-8 order-2 md:order-none">
       <h2 className="w-full text-3xl text-center">
-        GET EXCLUSIVE <strong>DEMO</strong> ACCESS!
+        GET EXCLUSIVE <strong>DEMO</strong> ACCESS SOON!
       </h2>
 
-      <p className="text-center py-2">
-        Unlock the exclusive demo, subscribe for updates, and stay in the loop
-        with the latest news!
+      <p className="text-center text-sm leading-6 px-1 py-2">
+        Sign up now for updates and be the first to know when our exclusive demo
+        is available! Stay in the loop with the latest news – don&apos;t miss
+        out!
       </p>
 
       <div className="rounded-lg">
