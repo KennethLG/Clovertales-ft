47382ca0df94bc938800a162e80ff34fd0f10fd2
KennethLG <orsonleiziaga@gmail.com>
2023-09-05 22:23:46 -0500

    Add node-fetch package and update the fetchLegalDocument function to fetch the legal document from the CDN and return it as text, then console.log the document and return it as a div with dangerouslySetInnerHTML in the Legal component

--- src/app/legal/page.tsx
+++ src/app/legal/page.tsx
@@ -1,19 +1,18 @@
-import { config } from "@/config"
+import { config } from "@/config";
+import fetch from "node-fetch";
 
 const fetchLegalDocument = async () => {
-  const response = await fetch(`${config.aws.api}`, {
-    next: {
-      revalidate: config.time.day
-    }
-  });
+  const response = await fetch(`${config.aws.cdn}/legal.html`);
 
-  const legalDocument = await response.json();
+  const legalDocument = await response.text();
   return legalDocument;
-}
+};
 
 const Legal = async () => {
   const legalDocument = await fetchLegalDocument();
-  console.log(legalDocument);
-}
+  console.log("document", legalDocument);
+
+  return <div dangerouslySetInnerHTML={{ __html: legalDocument }} />;
+};
 
 export default Legal;
