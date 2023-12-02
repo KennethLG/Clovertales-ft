0cec95e7bf17bcae08c3dce0a88e508ac4569892
KennethLG <orsonleiziaga@gmail.com>
2023-12-01 22:25:08 -0500

    Refactor ImagePopup component to conditionally render the popup based on the isOpen prop, and handle the transition and visibility of the popup in a more efficient way

--- src/components/global/Image/ImagePopup.tsx
+++ src/components/global/Image/ImagePopup.tsx
@@ -1,5 +1,6 @@
 import classNames from "classnames";
 import { Image } from "./Image";
+import { useEffect, useState } from "react";
 
 interface ImagePopupProps {
   src?: string;
@@ -9,36 +10,46 @@ interface ImagePopupProps {
 }
 
 export const ImagePopup = ({ src, alt, isOpen, onClose }: ImagePopupProps) => {
+  const [isRendered, setIsRendered] = useState(isOpen);
 
-  return (
-    <div
-      className={classNames(
-        "fixed top-0 left-0 w-screen h-screen flex justify-center items-center bg-black bg-opacity-80 z-50 transition-opacity duration-300 select-none",
-        {
-          "opacity-100": isOpen,
-          "opacity-0": !isOpen,
-          "pointer-events-none": !isOpen,
-        }
-      )}
-    >
-      <div className="relative w-5/6 h-5/6 pointer-events-none">
-        {src && alt ? (
+  useEffect(() => {
+    if (isOpen) {
+      setIsRendered(true);
+    } else {
+      const timer = setTimeout(() => setIsRendered(false), 300);
+      return () => clearTimeout(timer);
+    }
+  }, [isOpen]);
+
+  const popupClasses = classNames(
+    "fixed top-0 left-0 w-screen h-screen flex justify-center items-center bg-black bg-opacity-80 z-50 transition-opacity duration-300 select-none",
+    {
+      "opacity-0 pointer-events-none": !isOpen,
+      "opacity-100 pointer-events-auto": isOpen,
+    }
+  );
+
+  return isRendered ? (
+    <div className={popupClasses}>
+      <div className="relative w-5/6 h-5/6">
+        {src && alt && (
           <Image
             src={src}
             alt={alt}
             fill
             className="absolute top-0 left-0 w-full h-full object-contain"
           />
-        ) : (
-          <></>
         )}
         <button
-          onClick={onClose}
+          onClick={(e) => {
+            e.stopPropagation();
+            onClose();
+          }}
           className="absolute top-2 right-2 text-white font-bold text-2xl cursor-pointer"
         >
           X
         </button>
       </div>
     </div>
-  );
+  ) : null;
 };
