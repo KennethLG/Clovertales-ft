02c48bc8c60d548328553156cafb6d0941c8da61
KennethLG <orsonleiziaga@gmail.com>
2024-02-06 01:08:44 -0500

    Refactor FeaturedCard component to use Image component and LinkLayout, and add useImageClickNavigation hook for handling image click navigation

--- src/components/global/Card/FeaturedCard.tsx
+++ src/components/global/Card/FeaturedCard.tsx
@@ -1,7 +1,11 @@
-import Image from "next/image";
+"use client";
+
+import { Image } from "../Image/Image";
 import { formatDate } from "@/utils/date";
 import "./FeaturedCard.styles.css";
 import { truncateText } from "@/utils/text";
+import LinkLayout from "./LinkLayout";
+import useImageClickNavigation from "@/hooks/useImageClickNavigation";
 
 type FeaturedCardProps = {
   title: string;
@@ -21,28 +25,37 @@ export const FeaturedCard = ({
   date,
   url,
 }: FeaturedCardProps) => {
+  const { handleImageClick } = useImageClickNavigation(
+    url,
+    imageUrl,
+    `${title} image`
+  );
+
   return (
-    <div className="flex items-center space-x-4 rounded-lg overflow-hidden p-4">
-      <div className="flex-none w-24 h-24 relative">
-        <Image
-          src={imageUrl}
-          alt={`${title} image`}
-          width={96}
-          height={96}
-          className="rounded-lg"
-        />
+    <LinkLayout url={url}>
+      <div className="flex items-center space-x-4 rounded-lg overflow-hidden p-4">
+        <div className="flex-none w-24 h-24 relative">
+          <Image
+            src={imageUrl}
+            alt={`${title} image`}
+            width={96}
+            height={96}
+            className="rounded-lg"
+            onClick={handleImageClick}
+          />
+        </div>
+        <div className="flex-grow max-w-1">
+          <h5 className="text-sm leading-5">
+            {truncateText(title, maxTitleLength)}
+          </h5>
+          <p className="text-xs text-gray-400">
+            {truncateText(description, maxDescriptionLength)}
+          </p>
+          {date && (
+            <span className="text-sm text-gray-300">{formatDate(date)}</span>
+          )}
+        </div>
       </div>
-      <div className="flex-grow max-w-1">
-        <h5 className="text-sm leading-5">
-          {truncateText(title, maxTitleLength)}
-        </h5>
-        <p className="text-xs text-gray-400">
-          {truncateText(description, maxDescriptionLength)}
-        </p>
-        {date && (
-          <span className="text-sm text-gray-300">{formatDate(date)}</span>
-        )}
-      </div>
-    </div>
+    </LinkLayout>
   );
 };
